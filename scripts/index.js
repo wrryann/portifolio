737a86959d42257795c582ef438c2d89dac45463
wrryann <wrryann@gmail.com>
2025-07-26 13:38:57 -0300

    projects raw html and tests

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,35 @@
-let changeThemeBtn = document.getElementById("ChangeTheme")
-changeThemeBtn.addEventListener("click", () => {
-  console.log("Button was pressed!");
-});
+const languages = {
+  rust: {
+    name: 'Rust',
+    description: 'A programming language focused on safety and performance.',
+    link: 'https://www.rust-lang.org/',
+    color: 'rgba(237, 79, 26, 1)',
+  },
+  luau: {
+    name: 'Luau',
+    description: 'A programming language derived from Lua, used in Roblox.',
+    link: 'https://luau-lang.org/',
+    color: 'rgba(50, 81, 149, 1)',
+  }
+}
+
+class Project {
+  constructor(title, description, link, language,) {
+    this.title = title;
+    this.description = description;
+    this.link = link;
+    this.language = language;
+    
+
+  }
+
+  sayHello() {
+    console.log("Hi, I'm " + this.title);
+  }
+}
+
+const p1 = new Project("Ryan", 20);
+
+export function greet(number) {
+  return number + 65;
+}
