c2a21abdb1f5b62d69e76d72583e2ae593549086
wrryann <wrryann@gmail.com>
2025-07-29 23:15:17 -0300

    last commit before python

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,4 @@
-const languages = {
+export const languages = {
   rust: {
     name: 'Rust',
     description: 'A programming language focused on safety and performance.',
@@ -13,23 +13,60 @@ const languages = {
   }
 }
 
+const type = {
+  game: 'Game',
+  build: 'Build',
+  script: 'Script',
+  design: 'Design',
+  other: 'Other',
+}
+
+function getYouTubeThumbnail(videoUrl) {
+  const videoId = videoUrl.split("v=")[1]?.split("&")[0]; // extract the ID from the URL
+  if (!videoId) return null;
+  
+  return `https://img.youtube.com/vi/${videoId}/hqdefault.jpg`;
+}
+
+
 class Project {
-  constructor(title, description, link, language,) {
+  constructor(title, description, link, language, type) {
     this.title = title;
     this.description = description;
     this.link = link;
     this.language = language;
+    this.type = type;
+    this.thumbnail = getYouTubeThumbnail(link);
     
 
   }
 
   BuildHTML() {
+    let parent = document.querySelector('#project-grid');
+
+    let element = document.createElement('div');
+    element.classList.add('project');
+    parent.appendChild(element);
     
+    element.innerHTML = `
+      <img src="${this.thumbnail}" alt="">
+        <div class="programming-language" style="background-color: ${this.language.color};"> 
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M14.25 9.75 16.5 12l-2.25 2.25m-4.5 0L7.5 12l2.25-2.25M6 20.25h12A2.25 2.25 0 0 0 20.25 18V6A2.25 2.25 0 0 0 18 3.75H6A2.25 2.25 0 0 0 3.75 6v12A2.25 2.25 0 0 0 6 20.25Z" />
+          <p>${this.language.name}</p>
+        </svg>
+
+        </div>
+        <h3>${this.title}</h3>
+        <p>${this.description}</p>   
+      `
+
+      
   }
 }
 
-const p1 = new Project("Ryan", 20);
 
-export function greet(number) {
-  return number + 65;
+export function BuildProject(title, description, link, language, type) {
+  let project = new Project(title, description, link, language, type);
+  project.BuildHTML();
 }
